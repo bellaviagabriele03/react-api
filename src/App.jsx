a1f30e107c0511a97ed5f759a1abf8781b97ce7a
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-12-03 16:56:48 +0100

    Feat: milestone 1 completato

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   
   const [attori, setAttori] = useState([])
 
 
+  useEffect(() => {
+    axios.get("https://lanciweb.github.io/demo/api/actors/").then((resp) => {
+      setAttori(resp.data)
+      
+    })
+  }, [])
+
+  console.log(attori);
   
-
-
   return (
     <>
       
